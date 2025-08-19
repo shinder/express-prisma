f4c51a1171fdef0498d7146d1bdb5933b496042f
Shinder Lin <shinder.lin@gmail.com>
2025-08-19 22:19:36 +0800

    交易與批次操作

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -51,4 +51,56 @@ router.get("/delete/:user_id?", async (req: Request, res: Response) => {
     res.status(400).json({ error: e });
   }
 });
+
+router.get("/transaction-1", async (req: Request, res: Response) => {
+  try {
+    const result = await prisma.$transaction([
+      prisma.user.create({
+        data: {
+          firstName: "John",
+          lastName: "Doe",
+          email: "john1@example.com",
+        },
+      }),
+      prisma.user.create({
+        data: {
+          firstName: "Johnny",
+          lastName: "Doe",
+          email: "john2@example.com",
+        },
+      }),
+    ]);
+    res.json({ success: true, result });
+  } catch (error) {
+    console.error("交易失敗，資料已回滾");
+    res.status(400).json({ success: false, error });
+  }
+});
+router.get("/transaction-2", async (req: Request, res: Response) => {
+  let user, post;
+  try {
+    await prisma.$transaction(async (tx) => {
+      user = await tx.user.create({
+        data: {
+          firstName: "Shinder",
+          lastName: "Lin",
+          email: "shinder.lin@gmail.com",
+        },
+      });
+      post = await tx.post.create({
+        data: { content: "新貼文 1 內容", author_id: user.id },
+      });
+    });
+    res.json({ success: true, user, post });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
+});
+
+router.get("/shin", async (req: Request, res: Response) => {
+  const email = "shinder.lin@gmail.com";
+  const users = await prisma.$queryRaw`
+  SELECT * FROM users WHERE email=${email} `;
+  res.json(users);
+});
 export default router;
