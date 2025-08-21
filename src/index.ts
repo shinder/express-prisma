186a44c75311f04226819bbdaafe8bcd78f9bd20
Shinder Lin <shinder.lin@gmail.com>
2025-08-21 11:21:01 +0800

    首頁套用之前的 ejs 頁面

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import type { Request, Response, NextFunction } from "express";
 // 截入環境變數設定檔
 import "dotenv/config";
 
+import cookieParser from "cookie-parser";
+import session from "express-session";
+
 import usersRouter from "./routes/users";
 import tryABRouter from "./routes/try_ab";
 import apiContactsRouter from "./routes/api-contacts";
@@ -12,15 +15,54 @@ import apiContactsRouter from "./routes/api-contacts";
 
 // 建立伺服器主物件
 const app = express();
+app.set("view engine", "ejs");
+
 // 設定靜態內容資料夾
 app.use(express.static("public"));
 // 解析 JSON body 的中間件
 app.use(express.json());
 // 解析 URL-encoded body 的中間件
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser()); // 全域中介軟體：解析 cookies
+app.use(
+    session({
+      // 新用戶沒有使用到 session 物件時不會建立 session 和發送 cookie
+      saveUninitialized: false,
+      resave: false, // 沒變更內容是否強制回存
+      secret: "雜湊 session id 的字串",
+      // cookie: {
+      //   maxAge: 1200_000, // 20分鐘，單位毫秒
+      // },
+      // store: sessionStore, // 使用 MySQL 作為 session 儲存
+    })
+);
+
+
+// ************* 自訂的頂層 "中間件, 中介軟體" *************
+app.use((req, res, next) => {
+  res.locals.pageName = "";
+  res.locals.session = req.session; // 讓所有的 EJS 可以用 session 變數
+  res.locals.query = req.query;
+  res.locals.cookies = req.cookies;
+
+
+/*
+  const auth = req.get("Authorization");
+  if (auth && auth.indexOf("Bearer ") === 0) {
+    const token = auth.slice(7);
+    try {
+      req.my_jwt = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (ex) {}
+  }
+
+ */
+  next();
+});
+
 // 網站根目錄頁面
 app.get("/", (req: Request, res: Response) => {
-  res.send("歡迎來到 Express + TS !");
+  res.render("home", {name: 'prisma'});
 });
 
 app.use("/users", usersRouter);
